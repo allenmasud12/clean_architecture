c6bf3aa617df67b66aa18dde9544d8653ab6ed87
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-11-16 16:35:31 +0600

    feat:🎈 font manager and assets font

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+class FontConstants{
+  static const String fontFamily = "Montserrat";
+}
+class FontWeightManager{
+  static FontWeight light = FontWeight.w300;
+  static FontWeight regular = FontWeight.w400;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static double s12 = 12.0;
+  static double s14 = 14.0;
+  static double s16 = 16.0;
+  static double s17 = 17.0;
+  static double s18 = 18.0;
+  static double s20 = 20.0;
+}
