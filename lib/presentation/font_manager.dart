b7a7a15cd4bbcfab2b1cc1015fbaf5c42d646bfd
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-11-16 17:14:33 +0600

    feat:🎈style manager

--- lib/presentation/font_manager.dart
+++ lib/presentation/font_manager.dart
@@ -4,18 +4,18 @@ class FontConstants{
   static const String fontFamily = "Montserrat";
 }
 class FontWeightManager{
-  static FontWeight light = FontWeight.w300;
-  static FontWeight regular = FontWeight.w400;
-  static FontWeight medium = FontWeight.w500;
-  static FontWeight semiBold = FontWeight.w600;
-  static FontWeight bold = FontWeight.w700;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
 }
 
 class FontSize {
-  static double s12 = 12.0;
-  static double s14 = 14.0;
-  static double s16 = 16.0;
-  static double s17 = 17.0;
-  static double s18 = 18.0;
-  static double s20 = 20.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
 }
