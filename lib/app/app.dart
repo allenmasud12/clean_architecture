6ccdceb60c87675dc5fec733f286e51eb37fe07f
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-11-17 14:43:37 +0600

    feat:🎈splash screen and duration timer

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture/presentation/resources/routes_manager.dart';
 import 'package:clean_architecture/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,10 +1,50 @@
+import 'dart:async';
+
+import 'package:clean_architecture/presentation/resources/assets_manager.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../resources/color_manager.dart';
+import '../resources/routes_manager.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay(){
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext(){
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    _startDelay();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body:const Center(
+        child:  Image(
+          image: AssetImage(ImageAssets.splashLogo),
+        ),
+      ),
+    );
   }
 }
