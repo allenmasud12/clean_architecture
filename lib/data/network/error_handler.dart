e5ae517dc2c6a9d2775fe4331930c916e99b2b76
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-11-21 00:24:12 +0600

    feat:🎈error handle repository_impl

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -146,3 +146,8 @@ class ResponseMessage {
   static const String CACHE_ERROR = "Cache error, try again later";
   static const String NO_INTERNET_CONNECTION = "Please check your internet connection";
 }
+
+class ApiInternalStatus {
+  static const int SUCCESS = 0;
+  static const int FAILURE = 1;
+}
--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:clean_architecture/app/extension.dart';
 import 'package:clean_architecture/data/data_source/data_source.dart';
 import 'package:clean_architecture/data/mapper/mapper.dart';
+import 'package:clean_architecture/data/network/error_handler.dart';
 import 'package:clean_architecture/data/network/failure.dart';
 import 'package:clean_architecture/data/network/network_info.dart';
 import 'package:clean_architecture/data/request/request.dart';
@@ -8,26 +9,31 @@ import 'package:clean_architecture/domain/model.dart';
 import 'package:clean_architecture/domain/repository.dart';
 import 'package:dartz/dartz.dart';
 
-class RepositoryImpl extends Repository{
+class RepositoryImpl extends Repository {
   final RemoteDataSource _remoteDataSource;
   final NetworkInfo _networkInfo;
 
   RepositoryImpl(this._remoteDataSource, this._networkInfo);
 
   @override
-  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest) async{
-    if(await _networkInfo.isConnected){
-      //its safe to call API
-      final response = await _remoteDataSource.login(loginRequest);
-      if(response.status == 0){
-      return Right(response.toDomain());
-      }else{
-      return Left(Failure(409, response.message ?? "error!"));
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        //its safe to call API
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          return Right(response.toDomain());
+        } else {
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        //connection error
+        return (Left(ErrorHandler.handle(error).failure));
       }
     }else{
-      //connection error
-      return Left(Failure(501, "Please check your internet Connection."));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
-
 }
